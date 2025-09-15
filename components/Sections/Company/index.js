443dfb6f9a70533ee5d528f638b22f5f5970c62f
Tephii Noriega <devteph@gmail.com>
2025-09-15 01:10:36 -0600

    Fix Resolved

--- components/Sections/Company/index.js
+++ components/Sections/Company/index.js
@@ -1,7 +1,8 @@
-import React, { useEffect, useState } from 'react';
+import React, { useEffect, useRef } from 'react';
 import Image from 'next/image';
 
 function Locations  ({ }) {
+    const paragraphsRef = useRef([]);
 
     useEffect(() => {
         const observer = new IntersectionObserver(
